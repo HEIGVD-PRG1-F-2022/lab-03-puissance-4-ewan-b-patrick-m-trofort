6c1adce5ec741bb942726eff53ad5134ff0ce0ea
EwanHeig <ewan.bloechle@heig-vd.ch>
2022-10-18 15:35:25 +0200

    oui

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<vector<int>> tableau{{0,0,0,0},
+                                 {0,0,0,0},
+                                 {0,0,0,0},
+                                 {0,0,0,0},};
+
+
+    return 0;
+}
+
